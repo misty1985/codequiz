52adcbddd3c4cce09dd8f4d8e8769317706914da
sierra <haleyalton93@gmail.com>
2020-04-24 23:10:02 -0700

    a bit of a re work. having trouble connecting the javascript.

--- assets/quiz.js
+++ assets/quiz.js
@@ -1,152 +1,193 @@
-(function () {
-    var questions = [{
+function buildQuiz() {
+    const output = [];
+    myQuestions.forEach(
+        (currentQuestion, questionNumber) => {
+            const answers = [];
+            for (letter in currentQuestion.answers) {
+
+                answers.push(
+                    `<label>
+              <input type="radio" name="question${questionNumber}" value="${letter}">
+              ${letter} :
+              ${currentQuestion.answers[letter]}
+            </label>`
+                );
+            }
+            output.push(
+                `<div class="slide">
+            <div class="question"> ${currentQuestion.question} </div>
+            <div class="answers"> ${answers.join("")} </div>
+          </div>`
+            );
+        }
+    );
+    quizContainer.innerHTML = output.join('');
+}
+
+function showResults() {
+
+    const answerContainers = quizContainer.querySelectorAll('.answers');
+    let numCorrect = 0;
+
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+
+        const answerContainer = answerContainers[questionNumber];
+        const selector = `input[name=question${questionNumber}]:checked`;
+        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+        if (userAnswer === currentQuestion.correctAnswer) {
+            numCorrect++;
+
+            answerContainers[questionNumber].style.color = 'lightgreen';
+        }
+        else {
+            answerContainers[questionNumber].style.color = 'red';
+        }
+    });
+    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+}
+
+function showSlide(n) {
+    slides[currentSlide].classList.remove('active-slide');
+    slides[n].classList.add('active-slide');
+    currentSlide = n;
+    if (currentSlide === 0) {
+        previousButton.style.display = 'none';
+    }
+    else {
+        previousButton.style.display = 'inline-block';
+    }
+    if (currentSlide === slides.length - 1) {
+        nextButton.style.display = 'none';
+        submitButton.style.display = 'inline-block';
+    }
+    else {
+        nextButton.style.display = 'inline-block';
+        submitButton.style.display = 'none';
+    }
+}
+
+function showNextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function showPreviousSlide() {
+    showSlide(currentSlide - 1);
+}
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
+const myQuestions = [
+    {
         question: "About what year was Hogwarts founded?",
-        choices: [993, 1100, 1215, 901],
-        correctAnswer: 993
-    }, {
+        answers: {
+            a: "993",
+            b: "1100",
+            c: "1215",
+            d: "901"
+        },
+        correctAnswer: "a"
+    },
+    {
         question: "What position defends the goal post in quidditch?",
-        choices: ["Seeker", "Beater", "Chaser", "Keeper"],
-        correctAnswer: Keeper
+        answers: {
+            a: "Seeker",
+            b: "Beater",
+            c: "Chaser",
+            d: "Keeper"
+        },
+        correctAnswer: "d"
     }, {
         question: "Which school other than Durmstrang and Hogwarts participated in the Triwizard Cup?",
-        choices: ["Ilvermorny", "Beauxbatons", "Hufflepuff", "SLytherin"],
-        correctAnswer: "Beauxbatons"
+        answers: {
+            a: "Ilvermorny",
+            b: "Beauxbatons",
+            c: "Hufflepuff",
+            d: "SLytherin"
+        },
+        correctAnswer: "b"
     }, {
         question: "What is Alastor Moody's nickname?",
-        choices: ["Moody", "Padfoot", "Mad Eye", "Moody Pants"],
-        correctAnswer: "Mad Eye"
+        answers: {
+            a: "Moody",
+            b: "Padfoot",
+            c: "Mad Eye",
+            d: "Moody Pants"
+        },
+        correctAnswer: "c"
     }, {
         question: "Who hosted the Potterwatch pirate radio show in book 7?",
-        choices: ["Neville Longbottom", "Lee Jordan", "Draco Malfoy", "Ron Weasly"],
-        correctAnswer: "lee Jordan"
+        answers: {
+            a: "Neville Longbottom",
+            b: "Lee Jordan",
+            c: "Draco Malfoy",
+            d: "Ron Weasly"
+        },
+        correctAnswer: "b"
 
     }, {
         question: "What is the name of Harry Potters maternal aunt?",
-        choices: ["Marge Dursley", "Petunia Dursley", "Bellatrix Lestrange", "Molly Weasly"],
-        correctAnswer: "Petunia Dursley"
+        answers: {
+            a: "Marge Dursley",
+            b: "Petunia Dursley",
+            c: "Bellatrix Lestrange",
+            d: "Molly Weasly"
+        },
+        correctAnswer: "b"
     }, {
         question: "What is a non-magical person who has atleast one magical called?",
-        choices: ["Muggle", "Mudblood", "Squib", "Git"],
-        correctAnswer: "Squib"
+        answers: {
+            a: "Muggle",
+            b: "Mudblood",
+            c: "Squib",
+            d: "Git"
+        },
+        correctAnswer: "c"
 
     }, {
         question: "What are the followers of Lord Voldemort called?",
-        choices: ["The D.A", "The Order of The Phoenix", "Death Eaters", "Snatchers"],
-        correctAnswer: "Death Eaters"
+        answers: {
+            a: "The D.A",
+            b: "The Order of The Phoenix",
+            c: "Death Eaters",
+            d: "Snatchers"
+        },
+        correctAnswer: "c"
     }, {
         question: "What part of the Uk is Hogwarts located?",
-        choices: ["Ireland", "Wales", "Scotland", "Yorkshire"],
-        correctAnswer: "Scotland"
+        answers: {
+            a: "Ireland",
+            b: "Wales",
+            c: "Scotland",
+            d: "Yorkshire"
+        },
+        correctAnswer: "c"
     }, {
         question: "Where did Ron, Hermione, and Harry meet for the first time?",
-        choices: ["Diagon Alley", "Kings Cross Station", "Platform 9 and 3/4", "On the Hogwarts Express"],
-        correctAnswer: "On the Hodwarts Express"
+        answers: {
+            a: "Diagon Alley",
+            b: "Kings Cross Station",
+            c: "Platform 9 and 3/4",
+            d: "On the Hogwarts Express"
+        },
+        correctAnswer: "d"
     }, {
         question: "What is the name of Harry Potters's mother-in-law?",
-        choices: ["Narcissa Malfoy", "Molly Weasly", "Ginny Weasly", "Minerva McGonagall"],
-        correctAnswer: "Molly Weasly"
-    }];
-
-    var questionCounter = 0;
-    var selections = [];
-    var quiz = $('#quiz');
-
-    displayNext();
-    $('#next').on('click', function (e) {
-        e.preventDefault();
-        if (quiz.is(':animated')) {
-            return false;
-        }
-        choose();
-        if (isNaN(selections[questionCounter])) {
-            alert('Please make a selection!');
-        } else {
-            questionCounter++;
-            displayNext();
-        }
-    });
-
-    $('#start').on('click', function (e) {
-        e.preventDefault();
-
-        if (quiz.is(':animated')) {
-            return false;
-        }
-        questionCounter = 0;
-        selections = [];
-        displayNext();
-        $('#start').hide();
-    });
-
-    $('.button').on('mouseenter', function () {
-        $(this).addClass('active');
-    });
-    $('.button').on('mouseleave', function () {
-        $(this).removeClass('active');
-    });
-
-    function createQuestionElement(index) {
-        var qElement = $('<div>', {
-            id: 'question'
-        });
-
-        var header = $('<h2>Question ' + (index + 1) + ':</h2>');
-        qElement.append(header);
-
-        var question = $('<p>').append(questions[index].question);
-        qElement.append(question);
-
-        var radioButtons = createRadios(index);
-        qElement.append(radioButtons);
-
-        return qElement;
+        answers: {
+            a: "Narcissa Malfoy",
+            b: "Molly Weasly",
+            c: "Ginny Weasly",
+            d: "Minerva McGonagall"
+        },
+        correctAnswer: "b"
     }
+];
+buildQuiz();
+const nextButton = document.getElementById("next");
+const slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+showSlide(currentSlide);
 
-    function createRadios(index) {
-        var radioList = $('<ul>');
-        var item;
-        var input = '';
-        for (var i = 0; i < questions[index].choices.length; i++) {
-            item = $('<li>');
-            input = '<input type="radio" name="answer" value=' + i + ' />';
-            input += questions[index].choices[i];
-            item.append(input);
-            radioList.append(item);
-        }
-        return radioList;
-    }
+submitButton.addEventListener('click', showResults);
+nextButton.addEventListener("click", showNextSlide);
 
-    function choose() {
-        selections[questionCounter] = +$('input[name="answer"]:checked').val();
-    }
-
-
-    function displayNext() {
-        quiz.fadeOut(function () {
-            $('#question').remove();
-
-            if (questionCounter < questions.length) {
-                var nextQuestion = createQuestionElement(questionCounter);
-                quiz.append(nextQuestion).fadeIn();
-                if (!(isNaN(selections[questionCounter]))) {
-                    $('input[value=' + selections[questionCounter] + ']').prop('checked', true);
-                }
-
-            }
-            function displayScore() {
-                var score = $('<p>', { id: 'question' });
-
-                var numCorrect = 0;
-                for (var i = 0; i < selections.length; i++) {
-                    if (selections[i] === questions[i].correctAnswer) {
-                        numCorrect++;
-                    }
-                }
-
-                score.append('You got ' + numCorrect + ' questions out of ' +
-                    questions.length + ' right!!!');
-                return score;
-            }
-        })
-    }
-})
